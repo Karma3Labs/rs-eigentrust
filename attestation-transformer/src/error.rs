21777061a3c5afeb8909890d86aac68e2702fef0
Filip Lazovic <lazovicff@gmail.com>
2024-01-09 17:46:21 +0100

    Fix response messages

--- attestation-transformer/src/error.rs
+++ attestation-transformer/src/error.rs
@@ -1,11 +1,22 @@
+use std::num::TryFromIntError;
+
+use hex::FromHexError;
 use rocksdb::Error as RocksDbError;
 use secp256k1::Error as SecpError;
+use serde_json::Error;
 use thiserror::Error;
+use tonic::Status;
 
 #[derive(Debug, Error)]
 pub enum AttTrError {
-	#[error("SerialisationError")]
-	SerialisationError,
+	#[error("SerdeError: {0}")]
+	SerdeError(Error),
+
+	#[error("HexError: {0}")]
+	HexError(FromHexError),
+
+	#[error("TryFromError: {0}")]
+	TryFromError(TryFromIntError),
 
 	#[error("VerificationError: {0}")]
 	VerificationError(SecpError),
@@ -21,4 +32,10 @@ pub enum AttTrError {
 
 	#[error("NotImplemented")]
 	NotImplemented,
+}
+
+impl AttTrError {
+	pub fn into_status(self) -> Status {
+		Status::internal(format!("Internal error: {}", self))
+	}
 }
