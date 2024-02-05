23766ff480352c681e4efa1bfd8ca27fcac7ab60
Eugene Kim <ek@karma3labs.com>
2024-02-05 02:55:59 -0800

    Update go-eigentrust protobuf
    - Split into compute/trustvector/trustmatrix.
      This is so that the trustvector component can be used and implemented
      standalone, e.g. by snap-score-computer.
    - Link generated files into the proto-buf library crate.
      This was missing in the last PR.

--- proto-buf/build.rs
+++ proto-buf/build.rs
@@ -5,6 +5,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 	compile_protos("services/indexer.proto")?;
 	compile_protos("services/transformer.proto")?;
 	compile_protos("services/combiner.proto")?;
-	compile_protos("services/eigentrust.proto")?;
+	compile_protos("services/trustmatrix.proto")?;
+	compile_protos("services/trustvector.proto")?;
+	compile_protos("services/compute.proto")?;
 	Ok(())
 }
