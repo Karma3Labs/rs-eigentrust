711b2651edf3b0c9c5e28a54e1046da69b064711
Eugene Kim <ek@karma3labs.com>
2024-02-12 16:27:44 -0800

    Use rename_all=camelCase

--- snap-score-computer/src/vc.rs
+++ snap-score-computer/src/vc.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct StatusCredential {
 	#[serde(rename = "@context")]
 	pub context: Option<Vec<String>>,
@@ -8,28 +9,27 @@ pub struct StatusCredential {
 	#[serde(rename = "type")]
 	pub type_: String,
 	pub issuer: String,
-	#[serde(rename = "issuanceDate")]
 	pub issuance_date: Option<String>,
-	#[serde(rename = "credentialSubject")]
 	pub credential_subject: StatusCredentialSubject,
 	pub proof: StatusCredentialProof,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct StatusCredentialSubject {
 	pub id: String,
-	#[serde(rename = "currentStatus")]
 	pub current_status: String,
-	#[serde(rename = "statusReason")]
 	pub status_reason: Option<Vec<String>>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct StatusCredentialProof {
 	pub signature: String,
 }
 
 #[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct TrustScoreCredential {
 	#[serde(rename = "@context")]
 	pub context: Vec<String>,
@@ -37,23 +37,21 @@ pub struct TrustScoreCredential {
 	#[serde(rename = "type")]
 	pub type_: Vec<String>,
 	pub issuer: String,
-	#[serde(rename = "issuanceDate")]
 	pub issuance_date: String,
-	#[serde(rename = "credentialSubject")]
 	pub credential_subject: TrustScoreCredentialSubject,
 	pub proof: TrustScoreCredentialProof,
 }
 
 #[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct TrustScoreCredentialSubject {
 	pub id: String,
-	#[serde(rename = "trustScoreType")]
 	pub trust_score_type: String,
-	#[serde(rename = "trustScore")]
 	pub trust_score: TrustScore,
 }
 
 #[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct TrustScore {
 	pub value: f64,
 	#[serde(skip_serializing_if = "Option::is_none")]
@@ -61,16 +59,18 @@ pub struct TrustScore {
 }
 
 #[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct TrustScoreCredentialProof {}
 
 #[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Manifest {
 	pub issuer: String,
-	#[serde(rename = "issuanceDate")]
 	pub issuance_date: String,
 	pub locations: Option<Vec<String>>,
 	pub proof: ManifestProof,
 }
 
 #[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct ManifestProof {}
