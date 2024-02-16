72f06bb5bf9d8bddfc6de84056608c4e820ae0d3
Eugene Kim <ek@karma3labs.com>
2024-02-16 13:51:39 -0800

    Filter out library logs below WARN

--- indexer/src/logger/global.rs
+++ indexer/src/logger/global.rs
@@ -1,42 +1,48 @@
-use std::sync::Arc;
-use tracing_subscriber::FmtSubscriber;
-
 use tracing_subscriber::fmt::format::FmtSpan;
+use tracing_subscriber::FmtSubscriber;
 
 // move logger config here
 use crate::config::dotenv::LoggerConfig;
 
-pub struct AppLogger {
-	subscriber: Arc<FmtSubscriber>,
+#[derive(thiserror::Error, Debug)]
+pub enum LoggingError {
+	#[error("cannot load logging configuration from $SPD_IDX_LOG: {0}")]
+	BadEnv(tracing_subscriber::filter::FromEnvError),
 }
 
-impl AppLogger {
-	pub fn new(logger_config: LoggerConfig) -> Self {
-		// serialize structs with serde
-		// https://crates.io/crates/tracing-serde-structured
-		// slog logger, elastic
-		// https://github.com/graphprotocol/graph-node/blob/master/graph/src/log/mod.rs
+pub fn init(logger_config: LoggerConfig) -> Result<(), Box<dyn std::error::Error>> {
+	// serialize structs with serde
+	// https://crates.io/crates/tracing-serde-structured
+	// slog logger, elastic
+	// https://github.com/graphprotocol/graph-node/blob/master/graph/src/log/mod.rs
 
-		// todo more logger settings (transport etc)
-		// https://docs.rs/tracing-subscriber/latest/tracing_subscriber/fmt/struct.SubscriberBuilder.html#method.with_env_filter
-		let subscriber = FmtSubscriber::builder()
-			.with_file(false) // include file and line
-			.with_max_level(logger_config.logger_level)
-			.with_target(true) // include module path
-			.with_thread_ids(false)
-			.with_level(true)
-			.with_span_events(FmtSpan::CLOSE)
-			.with_ansi(true)
-			.finish();
-
-		AppLogger { subscriber: Arc::new(subscriber) }
-	}
-
-	pub fn init_global_default(&self) {
-		// colorful and formatted error reports
-		color_eyre::install().unwrap();
-
-		tracing::subscriber::set_global_default(self.subscriber.clone())
-			.expect("Failed to set the global tracing subscriber");
-	}
+	// todo more logger settings (transport etc)
+	// https://docs.rs/tracing-subscriber/latest/tracing_subscriber/fmt/struct.SubscriberBuilder.html#method.with_env_filter
+	let env_filter = tracing_subscriber::EnvFilter::builder()
+		.with_env_var("SPD_IDX_LOG")
+		.from_env()
+		.map_err(LoggingError::BadEnv)?
+		.add_directive(tracing_subscriber::filter::LevelFilter::WARN.into())
+		.add_directive(
+			format!(
+				"{}={}",
+				std::module_path!()
+					.split("::")
+					.next()
+					.expect("split should return at least one string"),
+				logger_config.logger_level.as_str()
+			)
+			.parse()
+			.expect("hard-coded filter is wrong"),
+		);
+	FmtSubscriber::builder()
+		.with_file(false) // include file and line
+		.with_env_filter(env_filter)
+		.with_target(true) // include module path
+		.with_thread_ids(false)
+		.with_level(true)
+		.with_span_events(FmtSpan::CLOSE)
+		.with_ansi(true)
+		.init();
+	Ok(())
 }
