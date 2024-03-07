21b8abcffb2c407189fc571b9cf2b7dc61fc9dd5
Eugene Kim <ek@karma3labs.com>
2024-03-06 22:47:30 -0800

    Make indexer run in CSV mode iff --csv=FILE

--- indexer/src/main.rs
+++ indexer/src/main.rs
@@ -1,14 +1,17 @@
 use clap::Parser;
 use tokio::time::Duration;
 
-use storage::lm_db::LMDBClient;
-
+use crate::clients::csv::client::CSVClient;
+use crate::clients::csv::types::CSVClientConfig;
 use crate::clients::metamask_connector::client::MetamaskConnectorClient;
 use crate::config::dotenv::Config;
 use crate::frontends::api::grpc_server::GRPCServer;
 use crate::logger::global::AppLogger;
+use crate::storage::lm_db::LMDBClient;
+use crate::tasks::csv_poc::task::CSVPOCTask;
 use crate::tasks::metamask_connector::task::MetamaskConnectorTask;
 use crate::tasks::service::TaskService;
+use crate::tasks::types::TaskTrait;
 
 pub mod cli;
 pub mod clients;
@@ -21,7 +24,7 @@ pub mod tasks;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let config = Config::from_env();
-	let _args = cli::Args::parse();
+	let args = cli::Args::parse();
 
 	let logger_config = config.logger_config.clone();
 	let logger: AppLogger = AppLogger::new(logger_config);
@@ -33,20 +36,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let lm_db_config = config.lm_db_config;
 	let db = LMDBClient::new(lm_db_config);
 
-	/*
-	   let csv_client_config = CSVClientConfig { path: _args.csv.clone() };
-	   let csv_client = CSVClient::new(csv_client_config);
-	   let csv_poc_task = CSVPOCTask::new(csv_client);
+	let task: Box<dyn TaskTrait> = if let Some(path) = args.csv {
+		let csv_client_config = CSVClientConfig { path };
+		let csv_client = CSVClient::new(csv_client_config);
+		Box::new(CSVPOCTask::new(csv_client))
+	} else {
+		let metamask_connector_client_config = config.metamask_connector_client_config;
+		let metamask_connector_client =
+			MetamaskConnectorClient::new(metamask_connector_client_config);
+		Box::new(MetamaskConnectorTask::new(metamask_connector_client))
+	};
 
-	   let task_service: TaskService = TaskService::new(Box::new(csv_poc_task), Box::new(db.clone()));
-	*/
-
-	let metamask_connector_client_config = config.metamask_connector_client_config;
-
-	let metamask_connector_client = MetamaskConnectorClient::new(metamask_connector_client_config);
-	let metamask_connector_task = MetamaskConnectorTask::new(metamask_connector_client);
-
-	let task_service = TaskService::new(Box::new(metamask_connector_task), Box::new(db.clone()));
+	let task_service = TaskService::new(task, Box::new(db.clone()));
 
 	let grpc_server_config = config.grpc_server_config;
 
